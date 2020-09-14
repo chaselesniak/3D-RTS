0314d58a0f949171d9990a41b2ae63082d2e8549
chaselesniak <chaselesniak>
2020-09-13 19:05:47 -0500

    3D RTS Project File

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public float panSpeed;
+    public float rotateSpeed;
+    public float rotateAmount;
+    private Quaternion rotation;
+
+    private float pandDetect = 15f;
+    private float minHeight = 10f;
+    private float maxHeight = 100f;
+    public GameObject selectedObject;
+    private ObjectInfo selectedInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = Camera.main.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+        RotateCamera();
+        if(Input.GetMouseButtonDown(0))
+        {
+            LeftClick();
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Camera.main.transform.rotation = rotation;
+        }
+    }
+
+
+    public void LeftClick()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit,100))
+        {
+            if(hit.collider.tag == "Ground")
+            {
+                selectedObject = null;
+                Debug.Log("Deselected");
+            }
+            else if(hit.collider.tag == "Selectable")
+            {
+                selectedObject = hit.collider.gameObject;
+                selectedInfo = selectedObject.GetComponent<ObjectInfo>();
+                selectedInfo.isSelected = true;
+                Debug.Log("selected");
+
+            }
+        }
+
+    }
+
+
+    void MoveCamera()
+    {
+        float moveX = Camera.main.transform.position.x;
+        float moveZ = Camera.main.transform.position.z;
+        float moveY = Camera.main.transform.position.y;
+        float xPos = Input.mousePosition.x;
+        float yPos = Input.mousePosition.y;
+
+        if(Input.GetKey(KeyCode.A)|| xPos > 0 && xPos < pandDetect)
+        {
+            moveX -= panSpeed;
+            
+        }
+        else if (Input.GetKey(KeyCode.D) || xPos < Screen.width && xPos > Screen.width-pandDetect)
+        {
+            moveX += panSpeed;
+
+        }
+         if (Input.GetKey(KeyCode.W) || yPos < Screen.height && yPos > Screen.height - pandDetect)
+        {
+            moveZ += panSpeed;
+
+        }
+        else if (Input.GetKey(KeyCode.S) || yPos > 0 && yPos < pandDetect)
+        {
+            moveZ -= panSpeed;
+
+        }
+        moveY -= Input.GetAxis("Mouse ScrollWheel")* (panSpeed *20);
+        moveY = Mathf.Clamp(moveY, minHeight, maxHeight);
+
+        Vector3 newPos = new Vector3(moveX, moveY, moveZ);
+        Camera.main.transform.position = newPos;
+    }
+
+
+    void RotateCamera()
+    {
+        Vector3 origin = Camera.main.transform.eulerAngles;
+        Vector3 destination = origin;
+        
+        if(Input.GetMouseButton(2))
+        {
+            destination.x -= Input.GetAxis("Mouse Y") * rotateAmount;
+            destination.y += Input.GetAxis("Mouse X") * rotateAmount;
+        }
+        if(destination != origin)
+        {
+            Camera.main.transform.eulerAngles = Vector3.MoveTowards(origin, destination, Time.deltaTime * rotateSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectInfo.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ObjectInfo : MonoBehaviour
+{
+
+
+    public NodeManager.ResourceTypes heldResourceType;
+
+    public bool isSelected = false;
+
+    public string objectName;
+
+    private NavMeshAgent agent;
+
+    public bool isGathering;
+
+    public int heldResource;
+
+    public int maxHeldResource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GatherTick());
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (heldResource >= maxHeldResource)
+        {
+            //drop off point here
+        }
+        if (Input.GetMouseButtonDown(1) && isSelected)
+        {
+            RightClick();
+        }
+    }
+
+
+    public void RightClick()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            if (hit.collider.tag == "Ground")
+            {
+                agent.destination = hit.point;
+
+            }
+            else if (hit.collider.tag == "Resource")
+            {
+                agent.destination = hit.collider.gameObject.transform.position;
+                Debug.Log("Harvesting");
+            }
+        }
+
+
+
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        GameObject hitObject = other.gameObject;
+        if (hitObject.tag == "Resource")
+        {
+            isGathering = true;
+            hitObject.GetComponent<NodeManager>().gatherers++;
+            heldResourceType = hitObject.GetComponent<NodeManager>().resourceType;
+        }
+    }
+
+
+    public void OnTriggerExit(Collider other)
+    {
+        GameObject hitObject = other.gameObject;
+
+        if (hitObject.tag == "Resource")
+        {
+            hitObject.GetComponent<NodeManager>().gatherers--;
+        }
+    }
+
+
+    IEnumerator GatherTick()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            if (isGathering)
+            {
+                heldResource++;
+            }
+            
+        }
+
+    }
+}
